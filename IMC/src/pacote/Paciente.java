ee8fa12fed942362d1834b10bd83e53675009923
Juliana Cavalaro de Oliveira <juliana.cavalaro@hotmail.com>
2024-03-12 20:10:19 -0300

    First

---
+++ IMC/src/pacote/Paciente.java
@@ -0,0 +1,19 @@
+package pacote;
+
+public class Paciente {
+	
+	double peso;
+	double altura;
+	
+	//Construtor
+	public Paciente(double peso, double altura) {
+		this.peso = peso;
+		this.altura = altura;
+	}
+	public double calculaIMC() {
+		double IMC =  peso / (altura * altura);
+	return IMC =0;
+	}
+	
+	
+}
