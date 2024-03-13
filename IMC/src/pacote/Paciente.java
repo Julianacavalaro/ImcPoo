5dc1ed249e0cabc3af5de79853c636d6b82a1573
Juliana Cavalaro de Oliveira <juliana.cavalaro@hotmail.com>
2024-03-13 20:47:14 -0300

    Finalização de atividade de POOImc

--- IMC/src/pacote/Paciente.java
+++ IMC/src/pacote/Paciente.java
@@ -1,19 +1,46 @@
 package pacote;
 
+//Classe paciente criada
 public class Paciente {
 	
+	//Ela possui atributos com a altura e o peso
 	double peso;
 	double altura;
+	double IMC;
 	
 	//Construtor
+	//O construtor da classe recebe essas informações como argumentos e os colocam nos respectivos atributos da classe
 	public Paciente(double peso, double altura) {
 		this.peso = peso;
 		this.altura = altura;
 	}
-	public double calculaIMC() {
-		double IMC =  peso / (altura * altura);
-	return IMC =0;
+	public double calculaIMC(Paciente p) {
+		double IMC;
+		IMC =  peso / (altura * altura);
+	return IMC;
 	}
-	
-	
+	public String diagnostico () {
+		if (IMC < 16) {
+			return "Baixo peso muito grave";
+		} else if (IMC < 16.99) {
+			return "Baixo peso grave";
+		} else if (IMC < 18.49){
+			return "Baixo peso grave";
+		} else if (IMC < 24.99){
+			return "Peso normal";
+		} else if (IMC < 29.99){
+			return "Sobrepeso ";
+		} else if (IMC < 34.99){
+			return "Obesidade grau I";
+		} else if (IMC < 39.99){
+			return "Obesidade grau II";
+		} else if (IMC >= 40){
+			return "Obesidade grau III (obesidade mórbida)";
+		}
+		return null;		
+		
+	}
+
 }
+	
+
